6b6bb11912f3869fd155a808b2d128e77de3c027
TuckTuckC <mrkittay847@gmail.com>
2021-07-20 12:35:50 -0400

    added firebase script in body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
       <h1>Sup Everybody</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button>Send Message</button>
+      {/* input field */}
+      {/* input button */}
+
+      {/* messages */}
+
     </div>
   );
 }
