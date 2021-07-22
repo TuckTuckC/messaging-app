9f9fb744fbf1702d7d03b0973896d06560781702
TuckTuckC <mrkittay847@gmail.com>
2021-07-22 04:40:36 -0400

    added a prompt to for username, and added username to message componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message.js'
 import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
 
-  console.log(input);
-  console.log(messages);
+  useEffect(() => {
+    setUsername(prompt('Please enter a name'));
+  }, [] );
 
   const sendMessage = (event) => {
     event.preventDefault();
@@ -18,20 +21,20 @@ function App() {
   return (
     <div className="App">
       <h1>Sup Everybody</h1>
+      <h2>Welcome {username}</h2>
 
       <form>
         <FormControl>
           <InputLabel>Enter a message...</InputLabel>
-          <Input  />
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
         </FormControl>
-        <input value={input} onChange={event => setInput(event.target.value)} />
-        <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
       </form>
 
       {/* messages */}
       {
         messages.map(message => (
-          <p>{message}</p>
+          <Message name={username} text={message} />
         ))
       }
     </div>
