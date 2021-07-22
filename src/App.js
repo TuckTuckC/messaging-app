ccdf9e40a726680f3dc99ff13c01947191e89320
TuckTuckC <mrkittay847@gmail.com>
2021-07-22 03:54:12 -0400

    added a button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
 function App() {
   const [input, setInput] = useState('');
 
+  console.log(input);
+
+  const sendMessage = (event) => {
+
+  }
+
   return (
     <div className="App">
       <h1>Sup Everybody</h1>
 
       <input value={input} onChange={event => setInput(event.target.value)} />
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
       {/* input field */}
       {/* input button */}
 
