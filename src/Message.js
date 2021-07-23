4d34a992f6f74d503fc54dad7016fd551885573c
TuckTuckC <mrkittay847@gmail.com>
2021-07-22 22:20:17 -0400

    added styles

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core';
+import React from 'react';
+import './Message.css';
 
 function Message(props) {
     return (
         <div>
-            <h2>{props.name}: {props.text}</h2>
+            <Card>
+                <CardContent>
+                    <Typography
+                    color='white'
+                    variant='h5'
+                    component='h2'
+                    >
+                        {props.name}: {props.text}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
