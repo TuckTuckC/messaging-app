6256466be27e1221536f534bcbb9677caf61f374
TuckTuckC <mrkittay847@gmail.com>
2021-07-27 12:02:36 -0400

    if messages are sent by you, they will appear on the right side now.

--- src/Message.js
+++ src/Message.js
@@ -3,16 +3,17 @@ import React from 'react';
 import './Message.css';
 
 function Message({message, name}) {
+    const isUser = name === message.username;
     return (
         <div>
-            <Card className="message">
+            <Card className={`message ${isUser && 'message_user'}`}>
                 <CardContent>
                     <Typography
                     color='white'
                     variant='h5'
                     component='h2'
                     >
-                        {message.name}: {message.text}
+                        {message.username}: {message.text}
                     </Typography>
                 </CardContent>
             </Card>
